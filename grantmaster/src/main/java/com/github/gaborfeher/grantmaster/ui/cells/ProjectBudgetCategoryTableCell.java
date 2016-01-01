16d9e58c3a56bd9844c1e40d371c71bc83b6b57c
Gabor Feher <feherga@gmail.com>
2016-01-01 18:03:09 +0100

    Only offer project-local categories for expenses (note: This requires the user to add limits before using a category. There is a quirk that removing the limit will not remove the category from the dropdown list)

---
+++ grantmaster/src/main/java/com/github/gaborfeher/grantmaster/ui/cells/ProjectBudgetCategoryTableCell.java
@@ -0,0 +1,49 @@
+/**
+ * This file is a part of GrantMaster.
+ * Copyright (C) 2015  Gábor Fehér <feherga@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.gaborfeher.grantmaster.ui.cells;
+
+import com.github.gaborfeher.grantmaster.framework.utils.DatabaseSingleton;
+import com.github.gaborfeher.grantmaster.logic.entities.BudgetCategory;
+import com.github.gaborfeher.grantmaster.framework.ui.cells.BetterChoiceBoxTableCell;
+import com.github.gaborfeher.grantmaster.framework.base.EditableTableRowItem;
+import com.github.gaborfeher.grantmaster.logic.entities.Project;
+import com.github.gaborfeher.grantmaster.logic.wrappers.ProjectBudgetCategoryWrapper;
+import com.github.gaborfeher.grantmaster.logic.wrappers.ProjectExpenseWrapper;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+class ProjectBudgetCategoryTableCell<S extends EditableTableRowItem>
+    extends BetterChoiceBoxTableCell<S, Object> {
+  public ProjectBudgetCategoryTableCell(String property) {
+    super(new BudgetCategoryStringConverter(), property, BudgetCategory.class);
+  }
+
+  @Override
+  protected void refreshChoiceItems() {
+    Project project = ((ProjectExpenseWrapper) getEntityWrapper()).getEntity().getProject();
+    DatabaseSingleton.INSTANCE.query((EntityManager em) -> {
+      List<ProjectBudgetCategoryWrapper> limits = ProjectBudgetCategoryWrapper.getProjectBudgetLimits(em, project, null);
+      getItems().clear();
+      for (ProjectBudgetCategoryWrapper projectBudgetCategory : limits) {
+        getItems().add(projectBudgetCategory.getBudgetCategory());
+      }
+      return true;
+    });
+  }
+
+}
